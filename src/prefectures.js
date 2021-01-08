40a24887621ca89d9c085289ce458116ff9d851a
rizzacarla <rizzacarla.12@gmail.com>
2021-01-08 04:36:14 -0800

    Add or delete event listeners for newly added data

---
+++ src/prefectures.js
@@ -0,0 +1,130 @@
+const itinerary = document.getElementById('itinerary')
+const regionPage = document.getElementById('region-page')
+
+const sapporoButton = document.getElementById('sapporo-city-button')
+const furanoButton = document.getElementById('furano-city-button')
+const hakodateButton = document.getElementById('hakodate-city-button')
+
+sapporoButton.addEventListener('click', (e) => {
+
+})
+
+furanoButton.addEventListener('click', (e) => {
+
+})
+
+hakodateButton.addEventListener('click', (e) => {
+
+})
+
+const akitaButton = document.getElementById('akita-city-button')
+const aomoriButton = document.getElementById('aomori-city-button')
+const sendaiButton = document.getElementById('sendai-city-button')
+
+akitaButton.addEventListener('click', (e) => {
+
+})
+
+aomoriButton.addEventListener('click', (e) => {
+
+})
+
+sendaiButton.addEventListener('click', (e) => {
+
+})
+
+const tokyoButton = document.getElementById('tokyo-city-button')
+const kanagawaButton = document.getElementById('kanagawa-city-button')
+const yokohamaButton = document.getElementById('yokohama-city-button')
+
+tokyoButton.addEventListener('click', (e) => {
+
+})
+
+kanagawaButton.addEventListener('click', (e) => {
+
+})
+
+yokohamaButton.addEventListener('click', (e) => {
+
+})
+
+const kanazawaButton = document.getElementById('kanazawa-city-button')
+const naganoButton = document.getElementById('nagano-city-button')
+const toyamaButton = document.getElementById('toyama-city-button')
+
+kanazawaButton.addEventListener('click', (e) => {
+
+})
+
+naganoButton.addEventListener('click', (e) => {
+
+})
+
+toyamaButton.addEventListener('click', (e) => {
+
+})
+
+const kyotoButton = document.getElementById('kyoto-city-button')
+const osakaButton = document.getElementById('osaka-city-button')
+const naraButton = document.getElementById('nara-city-button')
+
+kyotoButton.addEventListener('click', (e) => {
+
+})
+
+osakaButton.addEventListener('click', (e) => {
+
+})
+
+naraButton.addEventListener('click', (e) => {
+
+})
+
+const hiroshimaButton = document.getElementById('hiroshima-city-button')
+const okayamaButton = document.getElementById('okayama-city-button')
+const yonagoButton = document.getElementById('yonago-city-button')
+
+hiroshimaButton.addEventListener('click', (e) => {
+
+})
+
+okayamaButton.addEventListener('click', (e) => {
+
+})
+
+yonagoButton.addEventListener('click', (e) => {
+
+})
+
+const ehimeButton = document.getElementById('ehime-city-button')
+const kagawaButton = document.getElementById('kagawa-city-button')
+const kochiButton = document.getElementById('kochi-city-button')
+
+ehimeButton.addEventListener('click', (e) => {
+
+})
+
+kagawaButton.addEventListener('click', (e) => {
+
+})
+
+kochiButton.addEventListener('click', (e) => {
+
+})
+
+const nagasakiButton = document.getElementById('nagasaki-city-button')
+const kagoshimaButton = document.getElementById('kagoshima-city-button')
+const okinawaButton = document.getElementById('okinawa-city-button')
+
+nagasakiButton.addEventListener('click', (e) => {
+
+})
+
+kagoshimaButton.addEventListener('click', (e) => {
+
+})
+
+okinawaButton.addEventListener('click', (e) => {
+
+})
---
+++ src/region-info.js
@@ -0,0 +1,82 @@
+const itinerary = document.getElementById('itinerary')
+const regionPage = document.getElementById('region-page')
+const hokkaidoButton = document.getElementById('hokkaido-region-button')
+const tohokuButton = document.getElementById('tohoku-region-button')
+const kantoButton = document.getElementById('kanto-region-button')
+const chubuButton = document.getElementById('chubu-region-button')
+const kansaiButton = document.getElementById('kansai-region-button')
+const chugokuButton = document.getElementById('chugoku-region-button')
+const shikokuButton = document.getElementById('shikoku-region-button')
+const kyushuButton = document.getElementById('kyushu-region-button')
+
+hokkaidoButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block';
+  document.getElementById('hokkaido-prefecture').style.display = 'block';
+  document.getElementById('hokkaido-prefecture').style.display = 'flex';
+  document.getElementById('hokkaido-prefecture').style.justifyContent = 'center';
+  document.getElementById('hokkaido-prefecture').style.flexDirection = 'column';
+})
+
+tohokuButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('tohoku-prefecture').style.display = 'block';
+  document.getElementById('tohoku-prefecture').style.display = 'flex';
+  document.getElementById('tohoku-prefecture').style.justifyContent = 'center';
+  document.getElementById('tohoku-prefecture').style.flexDirection = 'column';
+})
+
+kantoButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('kanto-prefecture').style.display = 'block';
+  document.getElementById('kanto-prefecture').style.display = 'flex';
+  document.getElementById('kanto-prefecture').style.justifyContent = 'center';
+  document.getElementById('kanto-prefecture').style.flexDirection = 'column';
+})
+
+chubuButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('chubu-prefecture').style.display = 'block';
+  document.getElementById('chubu-prefecture').style.display = 'flex';
+  document.getElementById('chubu-prefecture').style.justifyContent = 'center';
+  document.getElementById('chubu-prefecture').style.flexDirection = 'column';
+})
+
+kansaiButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('kansai-prefecture').style.display = 'block';
+  document.getElementById('kansai-prefecture').style.display = 'flex';
+  document.getElementById('kansai-prefecture').style.justifyContent = 'center';
+  document.getElementById('kansai-prefecture').style.flexDirection = 'column';
+})
+
+chugokuButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('chugoku-prefecture').style.display = 'block';
+  document.getElementById('chugoku-prefecture').style.display = 'flex';
+  document.getElementById('chugoku-prefecture').style.justifyContent = 'center';
+  document.getElementById('chugoku-prefecture').style.flexDirection = 'column';
+})
+
+shikokuButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('shikoku-prefecture').style.display = 'block';
+  document.getElementById('shikoku-prefecture').style.display = 'flex';
+  document.getElementById('shikoku-prefecture').style.justifyContent = 'center';
+  document.getElementById('shikoku-prefecture').style.flexDirection = 'column';
+})
+
+kyushuButton.addEventListener('click', (e) => {
+  regionPage.style.display = 'none';
+  itinerary.style.display = 'block'
+  document.getElementById('kyushu-prefecture').style.display = 'block';
+  document.getElementById('kyushu-prefecture').style.display = 'flex';
+  document.getElementById('kyushu-prefecture').style.justifyContent = 'center';
+  document.getElementById('kyushu-prefecture').style.flexDirection = 'column';
+})
